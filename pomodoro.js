a967bd8cd4e79ced293debc816c80ba6bb6dc6b7
Palak <palakgupta2712@gmail.com>
2020-10-05 19:49:51 +0530

    alert message added

--- pomodoro.js
+++ pomodoro.js
@@ -12,7 +12,6 @@ function pomo(){
 }
 function short(){
     document.body.style.backgroundColor ="#7D7AA2";
-    
     timerElement.innerHTML = "05 : 00";
     time = 5;
     timer = time * 60;
@@ -21,24 +20,33 @@ function short(){
 function long(){
     document.body.style.backgroundColor ="#F67E7D";
     timerElement.innerHTML = "10 : 00";
-    time = 10;
+    time = 1;
     timer = time * 60;
 }
 function updateTimer(){
+    document.getElementById("progress").width = "400";
     let minutes = Math.floor(timer/60);
     let seconds = timer % 60 ;
     seconds = seconds < 10 ? '0' + seconds : seconds;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     timerElement.innerHTML = `${minutes} : ${seconds}`;
-    timer--;
-   
+    
+    timer--;  
+    if(timer==0){
+        document.getElementById("message").style.display = "block";
+        document.getElementById('message').innerHTML = "Session completed";
+        clearInterval(interval);
+    }
 }
 
 function startTimer(){
+    document.getElementById("message").style.display = "none";
     interval = setInterval(updateTimer,1000);
 }
 
 function pauseTimer(){
+    document.getElementById("message").style.display = "block";
+    document.getElementById('message').innerHTML = "Timer is paused";
     clearInterval(interval);
 }
 
