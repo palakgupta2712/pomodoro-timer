5908a3c76a5502f57a93f2b09c84a48800a9a8a4
Palak <palakgupta2712@gmail.com>
2020-10-05 18:44:39 +0530

    short_breaks and long_breaks timer added

--- pomodoro.js
+++ pomodoro.js
@@ -1,12 +1,30 @@
-const pomodoro = 25;
-let timer = pomodoro * 60;
+let time = 25;
+let timer = time * 60;
 let interval;
 
 const timerElement = document.getElementById('timer');
+
+function pomo(){
+    timerElement.innerHTML = "25 : 00";
+    time = 25;
+    timer = time * 60;
+}
+function short(){
+    timerElement.innerHTML = "05 : 00";
+    time = 5;
+    timer = time * 60;
+}
+
+function long(){
+    timerElement.innerHTML = "10 : 00";
+    time = 10;
+    timer = time * 60;
+}
 function updateTimer(){
-    const minutes = Math.floor(timer/60);
+    let minutes = Math.floor(timer/60);
     let seconds = timer % 60 ;
     seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
     timerElement.innerHTML = `${minutes} : ${seconds}`;
     timer--;
    
@@ -14,8 +32,8 @@ function updateTimer(){
 
 function startTimer(){
     interval = setInterval(updateTimer,1000);
-    
 }
+
 function pauseTimer(){
     clearInterval(interval);
 }
