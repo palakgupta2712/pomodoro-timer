ce809bd150973ed7e88f018b44317781ab0dd82a
Palak <palakgupta2712@gmail.com>
2020-10-05 21:34:06 +0530

    progress bar added

--- pomodoro.js
+++ pomodoro.js
@@ -1,17 +1,21 @@
 let time = 25;
 let timer = time * 60;
 let interval;
-
+let percentage;
 const timerElement = document.getElementById('timer');
 
 function pomo(){
     document.body.style.backgroundColor ="#6CC2BD";
+    document.getElementById("message").style.display = "none";
+    clearInterval(interval);
     timerElement.innerHTML = "25 : 00";
     time = 25;
     timer = time * 60;
 }
 function short(){
     document.body.style.backgroundColor ="#7D7AA2";
+    document.getElementById("message").style.display = "none";
+    clearInterval(interval);
     timerElement.innerHTML = "05 : 00";
     time = 5;
     timer = time * 60;
@@ -19,22 +23,28 @@ function short(){
 
 function long(){
     document.body.style.backgroundColor ="#F67E7D";
+    document.getElementById("message").style.display = "none";
+    clearInterval(interval);
     timerElement.innerHTML = "10 : 00";
-    time = 1;
+    time = 10;
+
     timer = time * 60;
 }
 function updateTimer(){
-    document.getElementById("progress").width = "400";
     let minutes = Math.floor(timer/60);
     let seconds = timer % 60 ;
     seconds = seconds < 10 ? '0' + seconds : seconds;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     timerElement.innerHTML = `${minutes} : ${seconds}`;
-    
+    percentage =100-((minutes/time )* 100);
+    console.log(percentage);
+    document.getElementById("progress").style.width =`${percentage}%`;
+
     timer--;  
-    if(timer==0){
+    
+    if(timer < 0){
         document.getElementById("message").style.display = "block";
-        document.getElementById('message').innerHTML = "Session completed";
+        document.getElementById('message').innerHTML = "Your session is completed";
         clearInterval(interval);
     }
 }
